5b9b253bf5bea184aab076a3122e5117c730a55c
shiyu <shiyu@.itheima>
2019-02-24 20:53:24 +0800

    我是石钰

---
+++ Service.java
@@ -0,0 +1,7 @@
+package cn.itcast.travel.domain;
+
+public class Service {
+    public static void main(String[] args) {
+        System.out.println("我是石钰");
+    }
+}
---
+++ hello.java
@@ -0,0 +1,9 @@
+package cn.itcast.travel.domain;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("大家好");
+
+        System.out.println("我是丁丁");
+    }
+}
